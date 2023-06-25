400f154f86a156064e1c3df4b626aa97f9c24132
Vishal Patil <vpatil.0496@gmail.com>
2023-06-25 14:16:36 +0530

    Added links to code in portfolio section.

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -8,14 +8,17 @@ const Portfolio = () => {
     {
       id: 1,
       src: IMDBClone,
+      href: "https://github.com/vishalpatil04/IMDB_Clone.git"
     },
     {
       id: 2,
       src: PersonalPortfolio,
+      href: "https://github.com/vishalpatil04/MyPortfolio.git"
     },
     {
       id: 3,
       src: TextUtils,
+      href: "https://github.com/vishalpatil04/IMDB_Clone.git"
     },
   ];
 
@@ -33,19 +36,19 @@ const Portfolio = () => {
         </div>
 
         <div className="grid w-full sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolios.map(({ id, src }) => (
+          {portfolios.map(({ id, src, href }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg ">
               <img
                 src={src}
                 alt=""
-                className="rounded-md duration-200 hover:scale-105"
+                className="rounded-md duration-100 hover:scale-105"
               />
               <div className="flex items-center justify-center">
                 <button className="w-1/2 px-6 py-3 duration-200 hover:scale-105">
                   Demo
                 </button>
                 <button className="w-1/2 px-6 py-3 duration-200 hover:scale-105">
-                  Code
+                  <a href={href} target="_blank" rel="noreferrer">Code</a>
                 </button>
               </div>
             </div>
