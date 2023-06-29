a02234c08a68b6acf3e493790aefb26ef8ce9c9e
Vishal Patil <vpatil.0496@gmail.com>
2023-06-29 18:11:39 +0530

    Updated Resume added

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -40,7 +40,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "/resume.pdf",
+      href: "/Vishal_React.pdf",
       style: "rounded-br-md",
       download: true,
     },
